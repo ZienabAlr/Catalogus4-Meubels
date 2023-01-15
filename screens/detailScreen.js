cfa5b7915a47be193441528f0622612018d0e5c0
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 21:54:05 +0100

    feat: detail component is imported

--- screens/detailScreen.js
+++ screens/detailScreen.js
@@ -1,3 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { StyleSheet, View, FlatList, TouchableOpacity, Text } from 'react-native';
 
+import detailComponent from "../components/detailComponent";
+
+
