c5522b71654c32fcfd98b93b89aa407136ac55a5
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 11:36:56 +0100

    fix: importing meubelScreen is fixed

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import MeubelScreen from "./screens/meubelScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import MeubelScreen from './screens/meubelSreen.js';
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
- 
+  return (
+    <View style={styles.container}>
+      <Text>Test2!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
