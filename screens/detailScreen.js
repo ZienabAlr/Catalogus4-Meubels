003787344b963f75f191c83e426e6414ee730b1b
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 21:57:07 +0100

    feat: text of detailscreen added

--- screens/detailScreen.js
+++ screens/detailScreen.js
@@ -3,4 +3,14 @@ import { StyleSheet, View, FlatList, TouchableOpacity, Text } from 'react-native
 
 import detailComponent from "../components/detailComponent";
 
+const detailScreen = ({navigation})=> {
+
+
+
+    return (
+        <View style={styles.container}>
+            <Text>{detailComponent}</Text>
+        </View>
+    ); 
+}
 
