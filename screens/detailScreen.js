141980fd58a046b1401e9b23d3fd8d65144539ac
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 21:58:43 +0100

    style: style added to the screen

--- screens/detailScreen.js
+++ screens/detailScreen.js
@@ -14,3 +14,12 @@ const detailScreen = ({navigation})=> {
     ); 
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+});
