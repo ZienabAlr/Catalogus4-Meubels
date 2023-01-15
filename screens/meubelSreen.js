27c420ca25717c0fee077f11a37313d1ec3c84c0
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 10:23:10 +0100

    style: style added to meubel screen

--- screens/meubelSreen.js
+++ screens/meubelSreen.js
@@ -37,3 +37,13 @@ const MeubelScreen = ({navigation})=> {
     }
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+    
