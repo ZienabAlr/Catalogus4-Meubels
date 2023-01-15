b300710b5132e1b600d6cc3d5e0bdacc519f010c
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 10:01:52 +0100

    style: style added to meubelComponent

--- components/meubelComponent.js
+++ components/meubelComponent.js
@@ -8,3 +8,12 @@ const MeubelComponent = props => {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
