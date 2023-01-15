dbeada9be5f92172fce12cc6916c4e8eaee8598a
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 10:10:10 +0100

    feat arrow function, and state are  added

--- screens/meubelSreen.js
+++ screens/meubelSreen.js
@@ -2,3 +2,8 @@ import React, {useState, useEffect} from "react";
 import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image } from 'react-native';
 
 import MeubelComponent from "../components/meubelComponent";
+
+const MeubelScreen = ({navigation})=> {
+
+    const [meubels, setMeubels] = useState([]);
+}
