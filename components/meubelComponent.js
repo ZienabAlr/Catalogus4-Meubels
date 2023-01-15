3b318fcc42618208e94a2356ce8b80ad22d84434
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 09:59:43 +0100

    feat: props added to get data from properties

--- components/meubelComponent.js
+++ components/meubelComponent.js
@@ -1,2 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+
+const MeubelComponent = props => {
+    return (
+        <View style={styles.container}>
+            <Text>{props.title}</Text>
+        </View>
+    );
+}
