9fdf9e2d41aaf3ed494958b6d0fde31ad2ac6750
ZienabAlr <r0784494@student.thomasmore.be>
2023-01-15 10:05:46 +0100

    feat: meubelComponent is imported

---
+++ screens/meubelSreen.js
@@ -0,0 +1,4 @@
+import React, {useState, useEffect} from "react";
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image } from 'react-native';
+
+import MeubelComponent from "../components/meubelComponent";
